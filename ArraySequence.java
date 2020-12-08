04d145b9d83652874297ef0c0c6dec7e6e90d308
wlin10 <wlin10@stuy.edu>
2020-12-08 12:44:00 -0500

    reset

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,27 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    for (int i = 0; i < other.length; i++) {
+      data[i] = other[i];
+    }
+  }
+
+  public void reset(){
+    this.currentIndex = 0;
+  }
+  public int length(){
+
+  }
+  public boolean hasNext(){
+
+  }
+  public int next(){
+    
+  }
+
+  public ArraySequence(IntegerSequence otherseq){
+
+  }
+
 }
